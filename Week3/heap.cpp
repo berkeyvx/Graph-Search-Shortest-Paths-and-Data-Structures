015350e4461fe4bbbd6b46cb0f0647cb43e2a77c
berkeyvx <iamberkeyavas@gmail.com>
2020-10-02 17:48:06 +0300

    Implementation of min heap

---
+++ Week3/heap.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <vector>
+#include <limits>
+
+class heap
+{
+	void swim(int idx);
+	void sink(int idx);
+public:
+	heap();
+	std::vector<int> h;
+	void insert(int i);
+	int extractMin();
+};
+
+
+heap::heap(){
+	h.push_back(INT_MIN); // arrary must be start at 1
+}
+
+
+void heap::insert(int i){
+	h.push_back(i);
+	swim(h.size()-1);
+
+}
+
+
+void heap::swim(int idx){
+	// parent -> idx/2
+	while(idx != 0 && h[idx/2] > h[idx]){
+		int temp = h[idx/2];
+		h[idx/2] = h[idx];
+		h[idx] = temp;
+		idx = idx/2;
+	}
+}
+
+
+
+int heap::extractMin(){
+
+	if (h.size() == 1)
+	{
+		h.pop_back();
+		return h.back();
+	}
+
+	if (h.size() <= 0)
+	{
+		return INT_MIN;
+	}
+
+	int min = h[1];
+	h[1] = h.back();
+	h.pop_back();
+	sink(1);
+	return min;
+}
+
+void heap::sink(int idx){
+	int l = 2*idx;
+	int r = 2*idx+1;
+	int smallest = idx;
+	while(idx < h.size()){
+		if (l < h.size() && h[l] < h[idx])
+		{
+			smallest = l;
+		}
+
+		if (r < h.size() && h[r] < h[smallest])
+		{
+			smallest = r;
+		}
+
+		if(smallest == idx){
+			break;
+		}
+
+		int temp = h[idx];
+		h[idx] = h[smallest];
+		h[smallest] = temp;
+		idx = smallest;
+	}
+}
+
+
+int main(int argc, char const *argv[])
+{
+	heap h;
+	for (std::vector<int>::iterator i = h.h.begin() + 1; i != h.h.end(); ++i)
+	{
+		std::cout << *i << " ";
+	}
+	return 0;
+}
